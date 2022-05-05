3b09a85ba58220c4ae38c201417c0d85fbb5aadc
Mandarina <idcareg.g@gmail.com>
2022-05-04 22:13:42 -0500

    Home 8

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_whatsapp/widgets/item_chat_widget.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -7,64 +8,9 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: 3,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            margin: const EdgeInsets.only(bottom: 8.0),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.black12,
-                radius: 26,
-                backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-                ),
-              ),
-              title: Text(
-                "Fátima de las Nieves",
-                style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              subtitle: Text(
-                "He enviado los archivos que solicitaste, por favor los revisas.",
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 13.0),
-              ),
-              trailing: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    "20:24",
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      // color: Colors.black.withOpacity(0.45)
-                      color: Color(0xff01C851),
-                    ),
-                  ),
-                  Container(
-                    height: 20,
-                    width: 20,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xff01C851),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      "7",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 10.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
+          return ItemChatWidget();
         },
       ),
     );
