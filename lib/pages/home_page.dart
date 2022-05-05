86ac656a4fb12a97f38d87261561e2c01e37a8b2
Mandarina <idcareg.g@gmail.com>
2022-05-04 20:48:44 -0500

    Home 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    print("INIT STATE");
+    _tabController = TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("BUILD!");
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.message),
@@ -26,7 +40,25 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              child: Text("CHATS", style: TextStyle(fontWeight: FontWeight.bold,),),
+            ),
+            Tab(
+              child: Text("STATUS", style: TextStyle(fontWeight: FontWeight.bold,),),
+            ),
+            Tab(
+              child: Text("CALLS", style: TextStyle(fontWeight: FontWeight.bold,),),
+            ),
+          ],
+        ),
       ),
     );
   }
+
 }
