1aa9ce6a5f3f0d1bc1bfc97eacb53e5248e95285
Mandarina <idcareg.g@gmail.com>
2022-05-04 22:30:15 -0500

    Home 9

---
+++ lib/data/data_dummy.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_codigo5_whatsapp/models/chat_model.dart';
+
+List<ChatModel> chatList = [
+  ChatModel(
+    avatarURL: "https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Ximena Lopez",
+    message: "Ya he enviando el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+];
