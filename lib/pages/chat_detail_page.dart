44293a7a8d52cde95538b5a900169521741d718c
Mandarina <idcareg.g@gmail.com>
2022-05-05 20:10:03 -0500

    Chat Detail 2

--- lib/pages/chat_detail_page.dart
+++ lib/pages/chat_detail_page.dart
@@ -18,25 +18,83 @@ class ChatDetailPage extends StatelessWidget {
             const SizedBox(
               width: 8.0,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Ximena Lopez"),
-                SizedBox(
-                  height: 3.0,
-                ),
-                Text(
-                  "Last seen today 11:39 AM",
-                  style: TextStyle(
-                    color: Colors.white60,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 13.0
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Ximena Lopez",
+                    style: TextStyle(
+                      fontSize: 18.0
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 3.0,
+                  ),
+                  Text(
+                    "Last seen today 11:39 AM",
+                    style: TextStyle(
+                        color: Colors.white60,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 13.0),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.videocam_rounded),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: 300,
+              height: 300,
+              color: Colors.deepPurple,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.greenAccent,
+                    ),
+                  ),
+                  Text("Hola"),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
